a4c77bc36b453cc794d8ae848541c5695d7afb59
JeffPedersen <Pedersenofficial@gmail.com>
2023-06-25 18:10:02 -0400

    initial EditCard

--- flashcardapp/src/components/acquisition/EditCard.jsx
+++ flashcardapp/src/components/acquisition/EditCard.jsx
@@ -2,14 +2,14 @@ import React, { useState, useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 
 export default function EditCard(props) {
+    const { id } = useParams();
     const { deck_id } = useParams();
-    const [deckCategory, setDeckCategory] = useState("");
     const [cardFront, setCardFront] = useState("");
     const [cardBack, setCardBack] = useState("");
 
     const navigate = useNavigate();
 
-    const url = `http://localhost:4040/deck/${deck_id}`;
+    const url = `http://localhost:4040/deck/create/${deck_id}`;
 
     const fetchDeck = async () => {
         const requestOption = {
@@ -21,9 +21,12 @@ export default function EditCard(props) {
         try {
             const res = await fetch(url, requestOption);
             const data = await res.json();
-            const { category } = data.getDeck;
+            const { front, back, deck } = data.getDeck;
 
-            setDeckCategory(category);
+            setCardFront(front);
+            setCardBack(back);
+            setDeckName(deck);
+
         } catch (error) {
             console.error(error);
         }
@@ -39,7 +42,9 @@ export default function EditCard(props) {
         e.preventDefault();
 
         let bodyOjb = JSON.stringify({
-            category: deckCategory,
+            deck: deckName,
+            front: cardFront,
+            back: cardBack,
         });
 
         const requestOption = {
@@ -58,28 +63,44 @@ export default function EditCard(props) {
             console.error(error);
         }
     }
-            return (
-                <>
-                    <h1 style={{ textAlign: "center", textDecoration: "underline" }}>
-                        Edit Deck
-                    </h1>
-                    <Container>
-                        <Row>
-                            <Col md="8">
-                                <Form onSubmit={handleSubmit}>
-                                    <FormGroup>
-                                        <Label>Category</Label>
-                                        <Input
-                                            value={deckCategory}
-                                            onChange={(e) => setDeckCategory(e.target.value)}
-                                            autoComplete="off"
-                                        />
-                                    </FormGroup>
-                                </Form>
-                            </Col>
-                        </Row>
-                    </Container>
-                </>
-            );
-        }
-
+    return (
+        <>
+          <h1 style={{ textAlign: "center", textDecoration: "underline" }}>
+            Edit Card
+          </h1>
+          <Container>
+            <Row>
+              <Col md="4">
+                <FullButton>
+                  <Button 
+                  color='info' 
+                  outline
+                  onClick={() => navigate(`/deck`)}>
+                  Back to decks</Button>
+                </FullButton>
+              </Col>
+              <Col md="8">
+                <Form onSubmit={handleSubmit}>
+                  <FormGroup>
+                    <Label>Front</Label>
+                    <Input
+                      value={cardFront}
+                      onChange={(e) => setCardFront(e.target.value)}
+                      autoComplete="off"
+                    />
+                  </FormGroup>
+                  <FormGroup>
+                    <Label>Front</Label>
+                    <Input
+                      value={cardBack}
+                      onChange={(e) => setCardBack(e.target.value)}
+                      autoComplete="off"
+                    />
+                  </FormGroup>
+                </Form>
+              </Col>
+            </Row>
+          </Container>
+        </>
+      );
+    }
