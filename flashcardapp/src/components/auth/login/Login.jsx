5003570a1b33d76a887e38debf4c6026d317d080
Rich <c0deyes@protonmail.com>
2023-06-28 18:04:46 -0400

    login and signup with M-ui logic needs to be checked over

--- flashcardapp/src/components/auth/login/Login.jsx
+++ flashcardapp/src/components/auth/login/Login.jsx
@@ -1,6 +1,24 @@
 import { useRef } from "react";
-import { Form, FormGroup, Input, Label, Button } from "reactstrap";
+// removed Button to used M-ui button
+import { Form, FormGroup, Input, Label } from "reactstrap";
 import { useNavigate } from "react-router-dom";
+// importing M-ui into the project
+import * as React from "react";
+import Button from "@mui/material/Button";
+
+import Avatar from "@mui/material/Avatar";
+
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
 export default function Login({ updateToken }) {
   // Refs
@@ -34,7 +52,7 @@ export default function Login({ updateToken }) {
   //         console.log(err);
   //     }
   // };
-
+  const defaultTheme = createTheme();
   //! Style 2
   async function handleSubmit(e) {
     e.preventDefault();
@@ -67,31 +85,66 @@ export default function Login({ updateToken }) {
 
   // Standard import from Unit7, day50
   return (
-    <>
-      <h1> Login </h1>
-      <Form onSubmit={handleSubmit}>
-        <FormGroup>
-          <Label for="exampleEmail">Email</Label>
-          <Input
-            innerRef={emailRef}
-            id="email"
-            type="email"
-            placeholder="exampleEmail"
-            autoComplete="off"
-          />
-        </FormGroup>
-        <FormGroup>
-          <Label for="examplePassword">Password</Label>
-          <Input
-            innerRef={passwordRef}
-            id="password"
-            type="password"
-            placeholder="Password"
-            autoComplete="off"
-          />
-        </FormGroup>
-        <Button type="submit">Login</Button>
-      </Form>
-    </>
+    <ThemeProvider theme={defaultTheme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Log in
+          </Typography>
+          <Box
+            component="form"
+            onSubmit={handleSubmit}
+            noValidate
+            sx={{ mt: 1 }}
+          >
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+              innerRef={emailRef}
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+              innerRef={passwordRef}
+            />
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Remember me"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Login
+            </Button>
+          </Box>
+        </Box>
+      </Container>
+    </ThemeProvider>
   );
 }
--- flashcardapp/src/components/auth/signup/Signup.jsx
+++ flashcardapp/src/components/auth/signup/Signup.jsx
@@ -1,11 +1,28 @@
 //importing the tags use by reactstrap
-import { FormGroup, Form, Row, Col, Label, Input, Button } from "reactstrap";
+// removed Button for M-ui Sake
+import { FormGroup, Form, Row, Col, Label, Input } from "reactstrap";
 // importing useRef
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Card, CardBody, CardTitle, CardSubtitle, CardText } from "reactstrap";
+//using M-ui for signup page "https://github.com/mui/material-ui/blob/v5.13.6/docs/data/material/getting-started/templates/sign-up/SignUp.js"
+import * as React from "react";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+//using M-ui for background color
 
-export default function Signup({updateToken}) {
+export default function Signup({ updateToken }) {
   // variables use for containing the useRef() method/functionality
   const firstNameRef = useRef();
   const lastNameRef = useRef();
@@ -13,6 +30,8 @@ export default function Signup({updateToken}) {
   const passwordRef = useRef();
 
   const navigate = useNavigate();
+  //Material UI
+  const defaultTheme = createTheme();
 
   // handleSubmit function use to do the CRUD method to signup
   async function handleSubmit(e) {
@@ -23,6 +42,8 @@ export default function Signup({updateToken}) {
     const lastName = lastNameRef.current.value;
     const email = emailRef.current.value;
     const password = passwordRef.current.value;
+
+    // This is where password checks would live
 
     let body = JSON.stringify({
       firstName,
@@ -59,66 +80,113 @@ export default function Signup({updateToken}) {
 
   return (
     <>
-      <Card
-        style={{
-          width: "18rem",
-          margin: "center",
-        }}
-      >
-        <img alt="Sample" src="https://picsum.photos/300/200" />
-        <CardBody>
-          <Form onSubmit={handleSubmit}>
-            <FormGroup>
-              <Label>First Name:</Label>
-              <Input innerRef={firstNameRef} autoComplete={"off"} />
-            </FormGroup>
-            <FormGroup>
-              <Label>Last Name:</Label>
-              <Input innerRef={lastNameRef} autoComplete={"off"} />
-            </FormGroup>
-            <FormGroup>
-              <Label>Email:</Label>
-              <Input innerRef={emailRef} type="email" autoComplete={"off"} />
-            </FormGroup>
-            <FormGroup>
-              <Label>Password</Label>
-              <Input
-                innerRef={passwordRef}
-                type="password"
-                autoComplete={"off"}
-              />
-            </FormGroup>
-            <Button>
-              <Button type="submit">Signup</Button>
-            </Button>
-          </Form>
-        </CardBody>
-      </Card>
+      <img alt="Sample" src="https://picsum.photos/300/200" />
+      <ThemeProvider theme={defaultTheme}>
+        <Container component="main" maxWidth="xs" onSubmit={handleSubmit}>
+          <CssBaseline />
+          <Box
+            sx={{
+              marginTop: 8,
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              Sign up
+            </Typography>
+            <Box
+              component="form"
+              noValidate
+              onSubmit={handleSubmit}
+              sx={{ mt: 3 }}
+            >
+              <Grid container spacing={2}>
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                    autoComplete="given-name"
+                    name="firstName"
+                    required
+                    fullWidth
+                    id="firstName"
+                    label="First Name"
+                    autoFocus
+                    innerRef={firstNameRef}
+                    autoComplete={"off"}
+                  />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                    required
+                    fullWidth
+                    id="lastName"
+                    label="Last Name"
+                    name="lastName"
+                    autoComplete="family-name"
+                    innerRef={lastNameRef}
+                    autoComplete={"off"}
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    required
+                    fullWidth
+                    id="email"
+                    label="Email Address"
+                    name="email"
+                    autoComplete="email"
+                    innerRef={emailRef}
+                    type="email"
+                    autoComplete={"off"}
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    required
+                    fullWidth
+                    name="password"
+                    label="Password"
+                    type="password"
+                    id="password"
+                    autoComplete="new-password"
+                    innerRef={passwordRef}
+                    pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
+                    title=">8 characters, one upper, and number"
+                    placeholder="Greater than 8 characters, at least one number, uppercase, lowercase, and symbol "
+                    autoComplete={"off"}
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <FormControlLabel
+                    control={
+                      <Checkbox value="allowExtraEmails" color="primary" />
+                    }
+                    label="I want to receive inspiration, marketing promotions and updates via email."
+                  />
+                </Grid>
+              </Grid>
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+              >
+                Sign Up
+              </Button>
+              <Grid container justifyContent="flex-end">
+                <Grid item>
+                  <Link href="#" variant="body2">
+                    Already have an account? Sign in
+                  </Link>
+                </Grid>
+              </Grid>
+            </Box>
+          </Box>
+        </Container>
+      </ThemeProvider>
     </>
   );
 }
-
-//! previously resided in auth.jsx, moved here and modified to class style. Sorry Rich.
-// return (
-//   <>
-//     <Card
-//       style={{
-//         width: "18rem",
-//         margin: "center",
-//       }}
-//     >
-//       <img alt="Sample" src="https://picsum.photos/300/200" />
-//       <CardBody>
-//         <CardTitle tag="h5"></CardTitle>
-//         <CardSubtitle className="mb-2 text-muted" tag="h6">
-//           <Signup updateToken={props.updateToken} />
-//         </CardSubtitle>
-//         <CardText>
-//           Some quick example text to build on the card title and make up the
-//           bulk of the card‘s content.
-//         </CardText>
-//         <Button>Button</Button>
-//       </CardBody>
-//     </Card>
-//   </>
-// );
